57afa0c74024f3b0b0af1f419f1e4753414d2f5b
WonJongLee <wj629@naver.com>
2016-08-24 13:15:55 +0900

    socket button test

--- test123/app/src/main/java/neighbor/com/test123/MainActivity.java
+++ test123/app/src/main/java/neighbor/com/test123/MainActivity.java
@@ -3,6 +3,7 @@ package neighbor.com.test123;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import java.io.DataInputStream;
@@ -16,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     //통신 변수들
     Socket socket;
-    private final String IP = "127.0.0.1"; //genymotion host
-    private final int PORT = 12345; // port number
+    final String IP = "127.0.0.1"; //genymotion host
+    final int PORT = 12345; // port number
+
 
     private InputStream is;
     private OutputStream os;
@@ -26,21 +28,23 @@ public class MainActivity extends AppCompatActivity {
 
     private Thread thread;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        sendData(new byte[]{
-                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+    byte[] b = new byte[]{
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
                 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19,
                 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29,
                 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
                 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49,
                 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59,
                 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69,
-        });
+    };
+    String s = "test1234";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         connectServer();
     }
 
@@ -82,14 +86,22 @@ public class MainActivity extends AppCompatActivity {
         public synchronized void run() {
             while (true) {
                 try {
-                    os.close();
-                    is.close();
-                    dos.close();
-                    dis.close();
-                    socket.close();
-                    break;
+                    byte[] b = new byte[4096];
+                    dis.read(b);
+
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    //e.printStackTrace();
+                    //status = false;
+                    try {
+                        os.close();
+                        is.close();
+                        dos.close();
+                        dis.close();
+                        socket.close();
+                        break;
+                    } catch (IOException e1) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
@@ -98,9 +110,30 @@ public class MainActivity extends AppCompatActivity {
     public void sendData(byte[] data) {        //메세지를 보내는 메서드
         try {
             dos.write(data);
+            Log.d("[sendData]", " send byte Data !!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void sendData(String str) {        //메세지를 보내는 메서드
+        try {
+            byte[] bb;
+            bb = str.getBytes("ksc5601");    //한글로 바꿔주는 작업
+            dos.write(bb); //.writeUTF(str);
+            Log.d("[sendData]", " send String Data !!");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public void btnClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                sendData(s);
+                break;
+            case R.id.button2:
+                sendData(b);
+                break;
+        }
+    }
 }
