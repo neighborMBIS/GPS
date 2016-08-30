5271fed64fedb175ba85835ab7f7e30b8dd99cfd
WonJongLee <wj629@naver.com>
2016-08-30 15:39:54 +0900

    make Convert CSV to SQLite program

---
+++ CSVtoSQLite/app/src/main/java/neighbor/com/csvtosqlite/CSV_Util/StationUtil.java
@@ -0,0 +1,66 @@
+package neighbor.com.csvtosqlite.CSV_Util;
+
+/**
+ * Created by user on 2016-08-29.
+ */
+public class StationUtil {
+
+    private String station_id;
+    private String station_nm;
+    private String admin_nm;
+    private String sido_cd;
+    private String localcoordinatesX;
+    private String localcoordinatesY;
+
+    public String getStation_id() {
+        return station_id;
+    }
+
+    public void setStation_id(String station_id) {
+        this.station_id = station_id;
+    }
+
+    public String getStation_nm() {
+        return station_nm;
+    }
+
+    public void setStation_nm(String station_nm) {
+        this.station_nm = station_nm;
+    }
+
+    public String getAdmin_nm() {
+        return admin_nm;
+    }
+
+    public void setAdmin_nm(String admin_nm) {
+        this.admin_nm = admin_nm;
+    }
+
+    public String getSido_cd() {
+        return sido_cd;
+    }
+
+    public void setSido_cd(String sido_cd) {
+        this.sido_cd = sido_cd;
+    }
+
+    public String getLocalcoordinatesX() {
+        return localcoordinatesX;
+    }
+
+    public void setLocalcoordinatesX(String localcoordinatesX) {
+        this.localcoordinatesX = localcoordinatesX;
+    }
+
+    public String getLocalcoordinatesY() {
+        return localcoordinatesY;
+    }
+
+    public void setLocalcoordinatesY(String localcoordinatesY) {
+        this.localcoordinatesY = localcoordinatesY;
+    }
+
+
+
+
+}
